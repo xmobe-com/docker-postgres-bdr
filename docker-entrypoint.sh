8a102363605ea146201463de41c5d42a8aac58fb
Jonathan Giannuzzi <jonathan@giannuzzi.be>
2015-10-23 15:32:49 +0200

    Update based on the latest changes to the official image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 set -e
 
+set_listen_addresses() {
+	sedEscapedValue="$(echo "$1" | sed 's/[\/&]/\\&/g')"
+	sed -ri "s/^#?(listen_addresses\s*=\s*)\S+/\1'$sedEscapedValue'/" "$PGDATA/postgresql.conf"
+}
+
 if [ "$1" = 'postgres' ]; then
+	mkdir -p "$PGDATA"
 	chown -R postgres "$PGDATA"
-	
+
 	chmod g+s /run/postgresql
-	chown -R postgres:postgres /run/postgresql
-	
-	if [ -z "$(ls -A "$PGDATA")" ]; then
+	chown -R postgres /run/postgresql
+
+	# look specifically for PG_VERSION, as it is expected in the DB dir
+	if [ ! -s "$PGDATA/PG_VERSION" ]; then
 		gosu postgres initdb
-		
-		sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
-		
-		# check password first so we can ouptut the warning before postgres
+
+		# check password first so we can output the warning before postgres
 		# messes it up
 		if [ "$POSTGRES_PASSWORD" ]; then
 			pass="PASSWORD '$POSTGRES_PASSWORD'"
@@ -27,58 +32,71 @@ if [ "$1" = 'postgres' ]; then
 				         Docker's default configuration, this is
 				         effectively any other container on the same
 				         system.
-				         
+
 				         Use "-e POSTGRES_PASSWORD=password" to set
 				         it in "docker run".
 				****************************************************
 			EOWARN
-			
+
 			pass=
 			authMethod=trust
 		fi
-		
+
+		{ echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA/pg_hba.conf"
+		{ echo; echo "shared_preload_libraries = 'bdr'";
+			echo "wal_level = 'logical'";
+			echo "track_commit_timestamp = on";
+			echo "max_wal_senders = 10";
+			echo "max_replication_slots = 10";
+			echo "max_worker_processes = 10";
+		} >> "$PGDATA"/postgresql.conf
+
+		# internal start of server in order to allow set-up using psql-client		
+		# does not listen on TCP/IP and waits until start finishes
+		gosu postgres pg_ctl -D "$PGDATA" \
+			-o "-c listen_addresses=''" \
+			-w start
+
 		: ${POSTGRES_USER:=postgres}
 		: ${POSTGRES_DB:=$POSTGRES_USER}
+		export POSTGRES_USER POSTGRES_DB
 
 		if [ "$POSTGRES_DB" != 'postgres' ]; then
-			gosu postgres postgres --single -jE <<-EOSQL
+			psql --username postgres <<-EOSQL
 				CREATE DATABASE "$POSTGRES_DB" ;
 			EOSQL
 			echo
 		fi
-		
+
 		if [ "$POSTGRES_USER" = 'postgres' ]; then
 			op='ALTER'
 		else
 			op='CREATE'
 		fi
 
-		gosu postgres postgres --single -jE <<-EOSQL
+		psql --username postgres <<-EOSQL
 			$op USER "$POSTGRES_USER" WITH SUPERUSER $pass ;
 		EOSQL
 		echo
-		
-		{ echo;
-      echo "host all all 0.0.0.0/0 $authMethod";
-      echo "host replication all 0.0.0.0/0 $authMethod";
-    } >> "$PGDATA"/pg_hba.conf
-		
-		{ echo;
-      echo "shared_preload_libraries = 'bdr'";
-      echo "wal_level = 'logical'";
-      echo "track_commit_timestamp = on";
-      echo "max_wal_senders = 10";
-      echo "max_replication_slots = 10";
-      echo "max_worker_processes = 10";
-    } >> "$PGDATA"/postgresql.conf
 
-		if [ -d /docker-entrypoint-initdb.d ]; then
-			for f in /docker-entrypoint-initdb.d/*.sh; do
-				[ -f "$f" ] && . "$f"
-			done
-		fi
+		echo
+		for f in /docker-entrypoint-initdb.d/*; do
+			case "$f" in
+				*.sh)  echo "$0: running $f"; . "$f" ;;
+				*.sql) echo "$0: running $f"; psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < "$f" && echo ;;
+				*)     echo "$0: ignoring $f" ;;
+			esac
+			echo
+		done
+
+		gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
+		set_listen_addresses '*'
+
+		echo
+		echo 'PostgreSQL init process complete; ready for start up.'
+		echo
 	fi
-	
+
 	exec gosu postgres "$@"
 fi
 
